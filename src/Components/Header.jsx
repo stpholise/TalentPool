8d488afa068fb7c267ef5fc205e63a9e9e7231b5
stpholise <stpholise@gmail.com>
2024-10-22 11:30:13 +0100

    make Profile button better

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,9 +2,9 @@ import Vector from '../assets/Vector.svg'
 import ExpandMore from '../assets/Expand_more.svg'
 import RectangleImg from '../assets/Rectangle 85.png'
 import Hamboger from '../assets/Group 50.svg'
+import UserCircle from '../assets/User_cicrle_duotone.svg'
 import { Link } from 'react-router-dom'
 import { useState} from 'react'
-
 import { PropTypes } from 'prop-types'
 
 
@@ -38,9 +38,10 @@ const Header = ({state , setState}) => {
   return (
     <header className="pageHeader">
         <div className="smallScreenMenu menuBtn">
-            <button className=" transBtn" onClick={handlemenuToggle}> <img src={Hamboger} alt="" style={{width:'35px', height:'35px'}}/></button>
+            <button className=" transBtn" onClick={handlemenuToggle}> <img src={Hamboger} alt="" style={{width:'24px', height:'24px'}}/></button>
             <button className='profDis ballCont' onClick={handleShow}>
-                 <span className={`ball ${show ? 'active' : ''} `}></span>
+                 <img src={UserCircle} className={`ball ${show ? 'active' : ''} `} />
+                 <img src={ExpandMore} className={`expan ${show ? 'active' : ''} `} />
             </button>
         </div>
         { show && <div className="profileOverlay" onClick={() => {setShow(false)}}></div>}
@@ -53,13 +54,13 @@ const Header = ({state , setState}) => {
             </div>
             <div className="notificationIcon">
                 <button className="notification transBtn">
-                <img src={Vector} alt="profile image" />
+                <img src={Vector} className='notifi' alt="profile image" />
                 </button>
             </div>
             <div className="profile">
                 <button className="profileBtn transBtn" onClick={handleToggle}> 
                     <div className="profileLft">
-                    <img src={RectangleImg} alt="profile image" />
+                    <img src={RectangleImg} alt="profile image" className='pimage' />
                     <div className="profileDetail">
                         <h5 className="profileName">Genesis Anosike</h5>
                         <p className="profileRole">Employee</p>
